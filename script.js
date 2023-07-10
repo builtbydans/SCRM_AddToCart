1a12b43c28b0ca7cbc3954a30ecd81d8910a821a
Danish Shafi <danish.shafi@live.co.uk>
2023-07-10 19:58:23 +0100

    comments added. DB is not writeable now due to permissions change

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
 import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
+// Importing the DB **NOTE, DB has been turned on private, so write is not possible**
 const appSettings = {
     databaseURL: "https://addtocart-87451-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 
+// Initialising app and using Firebase libs to set up handshake.
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
+// "shoppingList" is the DB setup in Firebase
 const shoppingListInDB = ref(database, "shoppingList")
 
+// DOM variables
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 const shoppingListEl = document.getElementById("shopping-list")
 
+// EventListener on Button that pushes input to the DB and clears the input
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
     push(shoppingListInDB, inputValue)
     clearInput();
 })
 
+// Retrieves items from DB if vals exist, loops and appends to list.
 onValue(shoppingListInDB, function(snapshot) {
   clearListEl();
 
@@ -34,6 +40,7 @@ onValue(shoppingListInDB, function(snapshot) {
   }
 })
 
+// Takes item from DB returned by onValue function and appends to list
 const appendToListItems = (item) => {
   let itemID = item[0];
   let itemValue = item[1];
@@ -41,6 +48,7 @@ const appendToListItems = (item) => {
   let newEl = document.createElement("li");
   newEl.textContent = itemValue;
 
+  // Allows user to remove items by doubleclick. remove method by Firebase.
   newEl.addEventListener("dblclick", function() {
     let exactLocationOfStoryInDB = ref(database, `shoppingList/${itemID}`);
     remove(exactLocationOfStoryInDB);
@@ -48,10 +56,12 @@ const appendToListItems = (item) => {
   shoppingListEl.append(newEl);
 }
 
+// Clears the shopping list which is called in the onValue function
 const clearListEl = () => {
   shoppingListEl.innerHTML = "";
 }
 
+// Clears the input field which is called in addButton function
 const clearInput = () => {
   inputFieldEl.value = "";
 };
